bf73649c0ae569958e640718fc95f89c4304ecd1
Alvaro <al7aro@gmail.com>
2020-06-28 19:07:12 +0200

    organizationChanges

--- srcs/get_next_line.c
+++ srcs/get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "../includes/get_next_line.h"
 
 int	get_next_line(int fd, char **line)
 {
